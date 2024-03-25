7324892e57eabcaab738edf4bd0c187992876f36
Tony Ding <tony.ding2002ding@gmail.com>
2024-03-25 20:48:35 +1100

    [TonyD] Fixed up strategy

--- main.py
+++ main.py
@@ -26,29 +26,42 @@ def bid_ask_spread(y, standev, t, k):
     return y * standev ** 2 * (T - t) + (2 / y) * math.log(1 + y / k)
 
 
+
 if __name__ == "__main__":
     # T = 1: the Terminal time, q starts at 0 , t is time, y = 0.1,
-    aud_usd_prices = get_prices("data/AUDUSD.csv")
+    prices = get_prices("data/AUDUSD.csv")
     # Start with buying 10 units of the original price
-    mid_pices = [(x["bid"] + x["ask"])/2 for x in aud_usd_prices]
+    mid_pices = [(x["bid"] + x["ask"]) / 2 for x in prices]
     inventory = 100 * (mid_pices[0])
     current_time = 0
-    standev = statistics.stdev(mid_pices)
-    y = 0.1
+    y = 0.5*(10**6)
     pnl = 0
 
     # So let's say the strat is: we sell 100 units of AUDUSD over the ask price
     # We buy 100 units of AUDUSD when less than the bid price.
     # Inventories will be in total amount of asset (not whole units, it will be e.g. 10 * 0.91 = 9.1)
 
-    for i in range(1, len(aud_usd_prices)):
-        t = float(len(aud_usd_prices) - i) / len(aud_usd_prices)
-        indiff_price = indifference_price(mid_pices[i], inventory, t, standev)
-        spread = bid_ask_spread(y, standev, t, 1) # I'll let k = 1 as a parameter, not sure what it should be
-        if indiff_price + spread >= aud_usd_prices[i]["ask"]:
-            inventory -= 100 * (indiff_price + spread/2)
-        elif indiff_price - spread <= aud_usd_prices[i]["bid"]:
-            inventory += 100 * (indiff_price - spread/2)
+    for i in range(1, len(prices)):
+        t = float(len(prices) - i) / len(prices)
+        stdev = 0 if i == 1 else statistics.stdev(mid_pices[:i]) #standard deviation up to a point
+        indiff_price = indifference_price(mid_pices[i], inventory, t, stdev) # TODO: do we calculate the indiff price on the current price? or the previous row?
+        spread = bid_ask_spread(y, stdev, t, k=1.5)  # k is set as 1.5 as a parameter
+        bid_price = prices[i]["bid"]
+        ask_price = prices[i]["ask"]
+        print(f"INDIFF_PRICE: {indiff_price}, SPREAD: {spread}, STANDARD DEVIATION: {stdev}")
+        print(f" Upper limit: {format(indiff_price + spread / 2, '.5f')} : ask price: {ask_price} || lower limit: {format(indiff_price - spread / 2, '.5f')} : bid price : {bid_price} \n")
 
-    print(f"\nFINAL INVENTORY IS: {inventory/(10**6)} million")
+        # Actual market making strategy
+        if prices[i]["ask"] >= indiff_price + (spread/2):
+            inventory -= 100 * (indiff_price + spread / 2)
+            pnl += 100 * (indiff_price + spread / 2)
 
+        elif prices[i]["bid"] <= indiff_price - (spread/2):
+            inventory += 100 * (indiff_price - spread / 2)
+            pnl -= 100 * (indiff_price - spread / 2)
+
+
+    print(f"\nFor AUD/USD using the market making strategy, the FINAL INVENTORY IS: {inventory}")
+    print(f"FINAL PnL for AUD/USD IS: {format(pnl, '.5f')}")
+
+    #TODO: run for crypto, and for all currency pairs
