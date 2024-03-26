8d7cb21da759a98c6788a2ca70154dfce1f00df5
Tony Ding <tony.ding2002ding@gmail.com>
2024-03-26 21:13:41 +1100

    [TonyD] Can now loop through entire folder for currency pair and print them all out

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import csv
 import statistics
+import os
 
 
 def get_prices(path):
@@ -26,42 +27,62 @@ def bid_ask_spread(y, standev, t, k):
     return y * standev ** 2 * (T - t) + (2 / y) * math.log(1 + y / k)
 
 
+def calculate_pnl(prices, mid_prices, inventory):
+    pnl = 0
+    for i in range(1, len(prices)):
+        t = float(len(prices) - i) / len(prices)
+        stdev = 0 if (i <= 4) else statistics.stdev(mid_prices[:i - 3])  # standard deviation up to a point
+        indiff_price = indifference_price(mid_prices[0], inventory, t,
+                                          stdev) if (i <= 4) else indifference_price(mid_prices[i - 4], inventory, t,
+                                                                                     stdev)  # get indiff prices from 3 rows ago
+        spread = bid_ask_spread(y, stdev, t, k=1.5)  # k is set as 1.5 as a parameter
+        bid_price = prices[i]["bid"]
+        ask_price = prices[i]["ask"]
+
+        # Actual market making strategy
+        if ask_price >= indiff_price + (spread / 2):  # We want the indiff price to be from 3 rows ago
+            inventory -= 100 * (indiff_price + spread / 2)
+            pnl += 100 * (indiff_price + spread / 2)
+
+        elif bid_price <= indiff_price - (spread / 2):  # indiff price has to be from 3 rows ago
+            inventory += 100 * (indiff_price - spread / 2)
+            pnl -= 100 * (indiff_price - spread / 2)
+    return pnl
+
 
 if __name__ == "__main__":
+
+    # Loops through all of the currency pairs and stores them in a dict
+    all_prices = {}
+    all_mid_prices = {}
+    for filepath in os.listdir(os.getcwd() + '/data'):
+        if os.path.isfile(os.getcwd() + '/data/' + filepath):
+            all_prices[str(filepath)] = get_prices('data/' + filepath)
+
+        all_mid_prices[str(filepath)] = [(x["bid"] + x["ask"]) / 2 for x in all_prices[str(filepath)]]
+
     # T = 1: the Terminal time, q starts at 0 , t is time, y = 0.1,
-    prices = get_prices("data/AUDUSD.csv")
+    prices = get_prices("data/EURUSD.csv")
     # Start with buying 10 units of the original price
-    mid_pices = [(x["bid"] + x["ask"]) / 2 for x in prices]
-    inventory = 100 * (mid_pices[0])
+    mid_prices = [(x["bid"] + x["ask"]) / 2 for x in prices]
+    inventory = 100 * (mid_prices[0])
     current_time = 0
-    y = 0.5*(10**6)
     pnl = 0
+
+    y = 0.5*(10**6)
 
     # So let's say the strat is: we sell 100 units of AUDUSD over the ask price
     # We buy 100 units of AUDUSD when less than the bid price.
     # Inventories will be in total amount of asset (not whole units, it will be e.g. 10 * 0.91 = 9.1)
 
-    for i in range(1, len(prices)):
-        t = float(len(prices) - i) / len(prices)
-        stdev = 0 if i == 1 else statistics.stdev(mid_pices[:i]) #standard deviation up to a point
-        indiff_price = indifference_price(mid_pices[i], inventory, t, stdev) # TODO: do we calculate the indiff price on the current price? or the previous row?
-        spread = bid_ask_spread(y, stdev, t, k=1.5)  # k is set as 1.5 as a parameter
-        bid_price = prices[i]["bid"]
-        ask_price = prices[i]["ask"]
-        print(f"INDIFF_PRICE: {indiff_price}, SPREAD: {spread}, STANDARD DEVIATION: {stdev}")
-        print(f" Upper limit: {format(indiff_price + spread / 2, '.5f')} : ask price: {ask_price} || lower limit: {format(indiff_price - spread / 2, '.5f')} : bid price : {bid_price} \n")
+    final_pnls = {}
+    for i in all_prices:
+        final_pnls[i] = calculate_pnl(all_prices[i], all_mid_prices[i], inventory)
 
-        # Actual market making strategy
-        if prices[i]["ask"] >= indiff_price + (spread/2):
-            inventory -= 100 * (indiff_price + spread / 2)
-            pnl += 100 * (indiff_price + spread / 2)
-
-        elif prices[i]["bid"] <= indiff_price - (spread/2):
-            inventory += 100 * (indiff_price - spread / 2)
-            pnl -= 100 * (indiff_price - spread / 2)
+    # final_pnl = calculate_pnl(prices, mid_prices, inventory)
+    # print(f" THE ACTUAL ONE FOR EUR/AUD.csv IS: {final_pnl}")
 
 
-    print(f"\nFor AUD/USD using the market making strategy, the FINAL INVENTORY IS: {inventory}")
-    print(f"FINAL PnL for AUD/USD IS: {format(pnl, '.5f')}")
-
-    #TODO: run for crypto, and for all currency pairs
+    print(f"If gamma was {y}: ")
+    for i in final_pnls:
+        print(f"The final Profit and Loss for: {i} is: {final_pnls[i]}")
