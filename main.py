97e28ee2327632644d53a2b0b7eb4fa2a1933c29
Tony Ding <tony.ding2002ding@gmail.com>
2024-03-27 00:53:24 +1100

    [TonyD] Fixed algorithm, added number of buys, sells and ending inventory

--- main.py
+++ main.py
@@ -31,10 +31,15 @@ def calculate_pnl(prices, mid_prices, inventory):
     pnl = 0
     num_buys = 0
     num_sells = 0
+    stdev_cache = [0] * len(prices)
 
     for i in range(1, len(prices)):
         t = float(len(prices) - i) / len(prices)
-        stdev = 0 if (i <= 4) else statistics.stdev(mid_prices[:i - 3])  # standard deviation up to a point
+
+        # Calculate standard deviation once and cache it
+        if stdev_cache[i] == 0 and i > 4:
+            stdev_cache[i] = statistics.stdev(mid_prices[:i - 3])
+        stdev = stdev_cache[i]
         indiff_price = indifference_price(mid_prices[0], inventory, t,
                                           stdev) if (i <= 4) else indifference_price(mid_prices[i - 4], inventory, t,
                                                                                      stdev)  # get indiff prices from 3 rows ago
@@ -52,12 +57,12 @@ def calculate_pnl(prices, mid_prices, inventory):
             inventory += 100 * (indiff_price - spread / 2)
             pnl -= 100 * (indiff_price - spread / 2)
             num_buys += 1
-    return {'pnl': pnl, 'num_buys': num_buys, 'num_sells': num_sells}
+    return {'pnl': pnl, 'num_buys': num_buys, 'num_sells': num_sells, 'inventory': inventory}
 
 
 if __name__ == "__main__":
 
-    # Loops through all of the currency pairs and stores them in a dict
+    # Loops through all the currency pairs and stores them in a dict
     all_prices = {}
     all_mid_prices = {}
     for filepath in os.listdir(os.getcwd() + '/data'):
@@ -66,32 +71,37 @@ if __name__ == "__main__":
 
         all_mid_prices[str(filepath)] = [(x["bid"] + x["ask"]) / 2 for x in all_prices[str(filepath)]]
 
-    # # T = 1: the Terminal time, q starts at 0 , t is time, y = 0.1,
-    # prices = get_prices("data/EURUSD.csv")
-    # # Start with buying 10 units of the original price
-    # mid_prices = [(x["bid"] + x["ask"]) / 2 for x in prices]
-
     current_time = 0
     pnl = 0
-    y = 50000
 
-    # So let's say the strat is: we sell 100 units of AUDUSD over the ask price
-    # We buy 100 units of AUDUSD when less than the bid price.
-    # Inventories will be in total amount of asset (not whole units, it will be e.g. 10 * 0.91 = 9.1)
+    # y = 50000
 
-    final_pnls = {}
-    final_buys = {}
-    final_sells = {}
-    for i in all_prices:
-        inventory = 100 * (all_mid_prices[i][0])
-        final_pnls[i] = calculate_pnl(all_prices[i], all_mid_prices[i], inventory)['pnl']
-        final_buys[i] = calculate_pnl(all_prices[i], all_mid_prices[i], inventory)['num_buys']
-        final_sells[i] = calculate_pnl(all_prices[i], all_mid_prices[i], inventory)['num_sells']
+    for y in range(10, 101, 10):
+        # y = 200000 # TODO: SWITCH TO THIS AND UNINDENT (SHIFT-TAB) IF YOU ONLY WANT TO TEST ONE GAMMA
+                    # TODO: ALSO, IF YOU ONLY WANT TO TEST ONE CSV FILE, DRAG IT OUT OF THE DATA FOLDER AND
+                    # INTO THE MAIN FOLDER
 
-    # final_pnl = calculate_pnl(prices, mid_prices, inventory)
-    # print(f" THE ACTUAL ONE FOR EUR/AUD.csv IS: {final_pnl}")
+        # So let's say the strat is: we sell 100 units of AUDUSD over the ask price
+        # We buy 100 units of AUDUSD when less than the bid price.
+        # Inventories will be in total amount of asset (not whole units, it will be e.g. 10 * 0.91 = 9.1)
+
+        final_pnls = {}
+        final_buys = {}
+        final_sells = {}
+        final_inventory = {}
+        for i in all_prices:
+            inventory = 100 * (all_mid_prices[i][0])
+            results = calculate_pnl(all_prices[i], all_mid_prices[i], inventory)
+            final_pnls[i] = results['pnl']
+            final_buys[i] = results['num_buys']
+            final_sells[i] = results['num_sells']
+            final_inventory[i] = results['inventory']
+
+        # final_pnl = calculate_pnl(prices, mid_prices, inventory)
+        # print(f" THE ACTUAL ONE FOR EUR/AUD.csv IS: {final_pnl}")
 
 
-    print(f"If gamma was {y}: ")
-    for i in final_pnls:
-        print(f"Profit and Loss for: {i} is: {final_pnls[i]} || Number of Buys: {final_buys[i]} || Number of Sells: {final_sells[i]}")
+        print(f"If gamma was {y}: ")
+        for i in final_pnls:
+            print(f"Profit and Loss for: {i} is: {final_pnls[i]} || Final Inventory : {final_inventory[i]} "
+                  f"|| Number of Buys: {final_buys[i]} || Number of Sells: {final_sells[i]}")
